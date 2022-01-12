49bb57667c15ecd8a7689444fafc5e5561e8aa47
locksten <email@locksten.com>
2022-01-13 01:02:24 +0200

    Fix date formatting an andrdoid

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,4 @@
-export const shortDate = (date: string | undefined) => {
-  if (!date) return undefined
-  const dateObj = new Date(date)
-  const month = dateObj?.toLocaleString("default", {
-    month: "short",
-  })
-  const year = dateObj?.toLocaleString("default", {
-    year: "numeric",
-  })
-  return `${year} ${month}`
-}
+import { format } from "date-fns"
+
+export const shortDate = (date: string | undefined) =>
+  date && format(new Date(date), "yyyy MMM")
